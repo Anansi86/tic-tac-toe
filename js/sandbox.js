66bcac2187eb514bf067063978701c3427343de0
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-10-06 18:27:04 +0000

    butto

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,38 +1,43 @@
-getBoardState = [
 
-    '', '', '',
-    '', '', '',
-    '', '', ''
+
+let boardState = [
+    'X','O','X',
+    'X','X','O',
+    '','','',
 ];
-
-winConditions [
-    [A1,A2,A3]     
-    [B1,B2,B3]
-    [C1,C2,C3] 
-    [A1,B1,C1] 
-    [A2,B2,C2]
-    [A3,B3,C3]
-    [A1,B2,C3]
-    [A3,B2,C1]
+let winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
 ]
 
+let container = document.createElement ("container")
+container.setAttribute("class", "container")
+document.getElementById("app").appendChild(container)
 
 
 
+let row = document.createElement ("div")
+row.setAttribute("class", "row")
+container.appendChild(row)
 
-let whosTurn = "X";
+let col1 = document.createElement("div");
+col1.setAttribute("class", "col");
+row.appendChild(col1);
 
-function playerTurn() {
-    
-for (var i = 0; i == whosTurn; i++) {  
-    if (whosTurn == "X") {
-    whosTurn = "O";
-} else {
-    whosTurn = "X";
-  
-}
-}
-//console.log(whosTurn)
-}
+let col2 = document.createElement("div");
+col2.setAttribute("class", "col");
+row.appendChild(col2);
 
-playerTurn();
+let col3 = document.createElement("div");
+col3.setAttribute("class", "col");
+row.appendChild(col3);
+
+
+let buttonInput = document.createElement("button");
+buttonInput.setAttribute("type", "submit");
+buttonInput.setAttribute("id", "btn")
+buttonInput.setAttribute("class", "btn btn-primary mb-2");
+buttonInput.innerText = "tic tac toe";
+col1.appendChild(buttonInput);
