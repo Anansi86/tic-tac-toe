9b6d677fb2bd703abe10a55adfea0e73e84b037c
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-10-07 00:02:50 +0000

    working buttons with id's

--- js/sandbox.js
+++ js/sandbox.js
@@ -9,48 +9,44 @@ let winConditions = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
+    [0,3,8],
+    [1,4,7],
+    [2,5,8],
     [0,4,8],
-]
+    [6,4,2]
+];
 
-let container = document.createElement ("container")
-container.setAttribute("class", "container")
-document.getElementById("app").appendChild(container)
+let container = document.createElement ("container");
+container.setAttribute("class", "container text-center");
+document.getElementById("app").appendChild(container);
 
 
 for (var i = 0; i < 3; i++) {
+    
+    
+    
+    let row = document.createElement ("div")
+    row.setAttribute("class", "row")
+    container.appendChild(row)
+    
+    
+    for (var j = 0; j < 3; j++) {
+    
+    let col1 = document.createElement("div");
+    col1.setAttribute("class", "col");
+    row.appendChild(col1);
+    
 
-let row = document.createElement ("div")
-row.setAttribute("class", "row")
-container.appendChild(row)
-
-let col1 = document.createElement("div");
-col1.setAttribute("class", "col");
-row.appendChild(col1);
-
-let col2 = document.createElement("div");
-col2.setAttribute("class", "col");
-row.appendChild(col2);
-
-let col3 = document.createElement("div");
-col3.setAttribute("class", "col");
-row.appendChild(col3);
-
+const buttonId = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let btnsomething = buttonId[3*i+j];
 
 let buttonInput1 = document.createElement("button");
-buttonInput1.setAttribute("id", "btn")
+buttonInput1.setAttribute("id", "btn");
 buttonInput1.setAttribute("class", "btn btn-primary mb-2");
-buttonInput1.innerText = "tic tac toe";
+buttonInput1.innerText = btnsomething;
+buttonInput1.setAttribute("id", btnsomething);
 col1.appendChild(buttonInput1);
 
-let buttonInput2 = document.createElement("button");
-buttonInput2.setAttribute("id", "btn")
-buttonInput2.setAttribute("class", "btn btn-primary mb-2");
-buttonInput2.innerText = "tic tac toe";
-col2.appendChild(buttonInput2);
 
-let buttonInput3 = document.createElement("button");
-buttonInput3.setAttribute("id", "btn")
-buttonInput3.setAttribute("class", "btn btn-primary mb-2");
-buttonInput3.innerText = "tic tac toe";
-col3.appendChild(buttonInput3);
+ } 
 }
