40ce53d3322c0d908400eb704188f16d44ff62e1
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-10-07 11:12:43 +0000

    blocker

--- js/sandbox.js
+++ js/sandbox.js
@@ -39,15 +39,48 @@ for (var i = 0; i < 3; i++) {
 
 const buttonId = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 
-let btnsomething = buttonId[3*i+j];
+let btnSomething = buttonId[3*i+j];
 
 let buttonInput1 = document.createElement("button");
 buttonInput1.setAttribute("id", "btn");
 buttonInput1.setAttribute("class", "btn btn-primary mb-2");
-buttonInput1.innerText = btnsomething;
-buttonInput1.setAttribute("id", btnsomething);
+buttonInput1.innerText = btnSomething;
+buttonInput1.setAttribute("id", btnSomething);
 col1.appendChild(buttonInput1);
 
 
- } 
+//var whosTurn = "X";
+
+/*function myFunction() {
+
+    if (btnSomething.innerHTML != "") {
+        return;
+    }
+
+    btnSomething.innerHTML = whosTurn;
+
+    if ((document.getElementById ("a1").innerHTML == whosTurn)
+     && (document.getElementById ("a2").innerHTML == whosTurn)
+      && (document.getElementById("a3").innerHTML == whosTurn)) {
+        console.log ("Winner: " + " " + whosTurn);
+ 
+     } else  {
+         console.log ("Draw")
+     }
+
+
+    if (whosTurn == "X") {
+        whosTurn = "O"
+    } else {
+        whosTurn = "X"
+    }
+
+
+    document.getElementById("btnSomething").innerHTML = whosTurn;
+    
+  }
+ }
+}
+*/
+ }
 }
